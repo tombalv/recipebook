0e8d9954bc9174278afa64ceb7529b8474ef2c7d
tomasbalvocius <tomasbalv@gmail.com>
2023-07-03 10:32:25 +0300

    recipe-book

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,15 @@
+import { Ingredient } from './../shared/ingredient.model';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css'],
+})
+export class ShoppingListComponent {
+  Ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+  constructor() {}
+}
